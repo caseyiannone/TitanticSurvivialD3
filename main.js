fbb2d54ec39568fba81048f3d2f8dfe3567c56ee
Casey Iannone <caseyiannone@users.noreply.github.com>
2017-04-23 19:51:36 -0400

    Add final D3 Project files

---
+++ main.js
@@ -0,0 +1,35 @@
+
+    var svg1 = dimple.newSvg("#chart1", 590, 400);
+    d3.csv("titanic_data.csv", function (data) {
+      var myChart = new dimple.chart(svg1, data);
+      myChart.setBounds(65, 45, 505, 315)
+      myChart.addCategoryAxis("x", "AgeGroup");
+      myChart.addPctAxis("y", "Passengers");
+      myChart.addSeries("Survived", dimple.plot.bar);
+      myChart.addLegend(200, 10, 380, 20, "right");
+      myChart.draw();
+    });
+
+
+    var svg2 = dimple.newSvg("#chart2", 590, 400);
+     d3.csv("titanic_data.csv", function (data) {
+       var myChart = new dimple.chart(svg2, data);
+       myChart.setBounds(60, 30, 510, 330)
+       myChart.addCategoryAxis("x", ["Sex", "Survived"]);
+       myChart.addMeasureAxis("y", "Passengers");
+       myChart.addSeries("Survived", dimple.plot.bar);
+       myChart.addLegend(65, 10, 510, 20, "right");
+       myChart.draw();
+     });
+
+
+     var svg3 = dimple.newSvg("#chart3", 590, 400);
+     d3.csv("titanic_data.csv", function (data) {
+       var myChart = new dimple.chart(svg3, data);
+       myChart.setBounds(65, 45, 505, 315)
+       myChart.addCategoryAxis("x", ["FareGroup", "Sex"]);
+       myChart.addPctAxis("y", "Passengers");
+       myChart.addSeries("Survived", dimple.plot.bar);
+       myChart.addLegend(200, 10, 380, 20, "right");
+       myChart.draw();
+     });
